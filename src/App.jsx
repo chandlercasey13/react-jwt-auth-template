8435f6c1481a86c41ee25486dc76ee900c9b859f
Chandler Casey <chandlercasey13@gmail.com>
2024-07-01 10:22:38 -0500

    added routing to signin and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import NavBar from './components/Navbar/Navbar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm' // import the SignupForm
+import SigninForm from './components/SigninForm/SigninForm'
+
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   return (
     <>
       <NavBar user={user} />
-      <h1>Hello world!</h1>
+      <Routes>
+        { user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+
+<Route path="/signup" element={<SignupForm setUser={setUser} />} />
+<Route path='/signin' element={<SigninForm setUser={setUser} />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
 export default App
